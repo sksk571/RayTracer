a38027eb87e4daa3dc9126e49e1ea51ecd563177
Sergey <Sergey@DESKTOP-KVV4R57>
2020-01-07 23:36:23 +0300

    Added reflection to dielectric

--- Dielectric.cs
+++ Dielectric.cs
@@ -14,28 +14,16 @@ namespace RayTracer
         public override bool Scatter(in Ray rIn, in HitRecord hit, out Vector3 attenuation, out Ray scattered)
         {
             attenuation = new Vector3(1.0f, 1.0f, 1.0f);
-            Vector3 reflected = VectorUtil.Reflect(rIn.Direction, hit.Normal);
-            Vector3 outwardNormal;
-            float niOverNt;
-            if (Vector3.Dot(rIn.Direction, hit.Normal) > 0)
-            {
-                outwardNormal = -hit.Normal;
-                niOverNt = refIndex;
-            }
-            else
-            {
-                outwardNormal = hit.Normal;
-                niOverNt = 1.0f / refIndex;
-            }
-            Vector3 refracted = VectorUtil.Refract(rIn.Direction, outwardNormal, niOverNt);
-            if (refracted != Vector3.Zero)
+            Vector3 refracted = VectorUtil.Refract(rIn.Direction, hit.Normal, refIndex);
+            if (refracted != Vector3.Zero &&
+                Util.Rand() < VectorUtil.Schlick(rIn.Direction, hit.Normal, refIndex))
             {
                 scattered = new Ray(hit.P, refracted);
             }
             else
             {
+                Vector3 reflected = VectorUtil.Reflect(rIn.Direction, hit.Normal);
                 scattered = new Ray(hit.P, reflected);
-                //return false;
             }
             return true;
         }
--- VectorUtil.cs
+++ VectorUtil.cs
@@ -10,16 +10,46 @@ namespace RayTracer
             return v - 2 * Vector3.Dot(v, normal) * normal;
         }
 
-        public static Vector3 Refract(in Vector3 v, in Vector3 normal, float niOverNt)
+        public static Vector3 Refract(in Vector3 v, in Vector3 normal, float refIndex)
         {
             Vector3 uv = Vector3.Normalize(v);
+            Vector3 outwardNormal;
             float dt = Vector3.Dot(uv, normal);
+            float niOverNt;
+            if (dt > 0)
+            {
+                outwardNormal = -normal;
+                niOverNt = refIndex;
+            }
+            else
+            {
+                outwardNormal = normal;
+                niOverNt = 1.0f / refIndex;
+            }
+
             float discriminant = 1.0f - niOverNt*niOverNt*(1-dt*dt);
             if (discriminant > 0)
             {
-                return niOverNt * (uv - normal * dt) - normal * (float)Math.Sqrt(discriminant);
+                return niOverNt * (uv - outwardNormal * dt) - outwardNormal * (float)Math.Sqrt(discriminant);
             }
             return Vector3.Zero;
+        }
+
+        public static float Schlick(in Vector3 v, in Vector3 normal, float refIndex)
+        {
+            float dt = Vector3.Dot(v, normal);
+            float cosine;
+            if (dt > 0)
+            {
+                cosine = refIndex*dt/v.Length();
+            }
+            else
+            {
+                cosine = -dt/v.Length();
+            }
+            float r0 = (1-refIndex) / (1+refIndex);
+            r0 = r0 * r0;
+            return r0 + (1-r0) * (float)Math.Pow((1-cosine), 5);
         }
     }
 }
